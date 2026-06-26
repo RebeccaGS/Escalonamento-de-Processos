6d207ce7c3893f6fbc5645b5f15562186a14e3d3
DanielNimbus <danielra@dcc.ufrj.br>
2026-06-26 19:45:33 -0300

    vou sair mas tá iniciado

--- Main.c
+++ Main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum{
+  ALTA, BAIXA
+} Prioridade;
+
+typedef enum{
+  PRONTO, EXEC, BLOQUEADO
+}Status;
+
+typedef struct
+{
+  int PID;
+  int PPID;
+  Prioridade prioridade; 
+  Status status;
+}Processo;
+
+Processo iniciaProcesso(int pid, int ppid){
+
+}
+
 void main(){
   printf("Hello world");
 }
